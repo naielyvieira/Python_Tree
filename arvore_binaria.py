c604b16bea38f62289d18e4f25718e7edc2f704f
naielyvieira <naielyvieira100@gmail.com>
2023-08-28 16:49:05 -0300

    Arvore Binaria

--- arvore_binaria.py
+++ arvore_binaria.py
@@ -1,94 +1,108 @@
 from queue import Queue
-class Arvore_Binaria:
-    def __init__(self, valor):
+class No:
+    def __init__(self, valor = None):
         self.valor = valor
         self.esquerda = None
         self.direita = None
-
-    def inserir_esquerda(self, valor):
-        if self.esquerda == None:
-            self.esquerda = Arvore_Binaria(valor)
-            
+        
+class Arvore_Binaria:
+    def __init__(self):
+        self.raiz = None
+        
+    def inserir_em_nivel(self, valor):
+        if self.raiz is None:
+            self.raiz = No(valor)
         else:
-            novo_no = Arvore_Binaria(valor)
-            novo_no.esquerda = self.esquerda
-            self.esquerda = novo_no
-            
-    def inserir_direita(self, valor):
-        if self.direita == None:
-            self.direita = Arvore_Binaria(valor)
-            
+            self._inserir_em_nivel_recursivo(valor, self.raiz)
+
+    def _inserir_em_nivel_recursivo(self, valor, no_atual):
+        if valor < no_atual.valor:
+            if no_atual.esquerda is None:
+                no_atual.esquerda = No(valor)
+            else:
+                self._inserir_em_nivel_recursivo(valor, no_atual.esquerda)
         else:
-            novo_no = Arvore_Binaria(valor)
-            novo_no.direita = self.direita
-            self.direita = novo_no        
-     
-    def pre_order(self):
-       print(self.valor)
-       if self.esquerda:
-        self.esquerda.pre_order()
-        
-       if self.direita:
-        self.direita.pre_order()
-        
-    def in_order(self):
-        if self.esquerda:
-            self.esquerda.in_order()
-        print(self.valor)
-
-        if self.direita:
-            self.direita.in_order()
+            if no_atual.direita is None:
+                no_atual.direita = No(valor)
+            else:
+                self._inserir_em_nivel_recursivo(valor, no_atual.direita)
+                
+    def mostrar_pre_ordem(self):
+        if self.raiz is None:
+            print('A árvore está vazia!')
+        else:
+            self.mostrar_pre_ordem_recursivo(self.raiz)
             
-    def pos_order(self):
-        if self.esquerda:
-            self.esquerda.pos_order()
+    def mostrar_pre_ordem_recursivo(self, no):
+        print(no.valor, end=" ")
+        if no.esquerda is not None:
+            self.mostrar_pre_ordem_recursivo(no.esquerda)
+        if no.direita is not None:
+            self.mostrar_pre_ordem_recursivo(no.direita)
+            
+    def mostrar_in_ordem(self):
+        if self.raiz is None:
+            print('A árvore está vazia!')
+        else:
+            self.mostrar_in_ordem_recursivo(self.raiz)
+    
+    def mostrar_in_ordem_recursivo(self, no):
+        if no.esquerda is not None:
+            self.mostrar_in_ordem_recursivo(no.esquerda)
+        print(no.valor, end=" ")
+        if no.direita is not None:
+            self.mostrar_in_ordem_recursivo(no.direita)
+            
+    def mostrar_pos_ordem(self):
+        if self.raiz is None:
+            print('A árvore está vazia!')
+        else:
+            self.mostrar_pos_ordem_recursivo(self.raiz)
+    
+    def mostrar_pos_ordem_recursivo(self, no):
+        if no.esquerda is not None:
+            self.mostrar_pos_ordem_recursivo(no.esquerda)
+        if no.direita is not None:
+            self.mostrar_pos_ordem_recursivo(no.direita)
+        print(no.valor, end=" ")
 
-        if self.direita:
-            self.direita.pos_order()
-        print(self.valor)
-        
     def bfs(self):
+        if self.raiz is None:
+            print('A árvore está vazia!')
+            return
+        
         fila = Queue()
-        fila.put(self)
-
+        fila.put(self.raiz)
+        
         while not fila.empty():
             no_atual = fila.get()
-            print(no_atual.valor)
+            print(no_atual.valor, end=" ")
 
-            if no_atual.esquerda:
+            if no_atual.esquerda is not None:
                 fila.put(no_atual.esquerda)
 
-            if no_atual.direita:
-                fila.put(no_atual.direita)    
-  
-# Criando uma árvore binária
-raiz = Arvore_Binaria(1)
-raiz.inserir_esquerda(2)
-raiz.inserir_direita(3)
-raiz.esquerda.inserir_esquerda(4)
-raiz.esquerda.inserir_direita(5)
-raiz.direita.inserir_esquerda(6)
-raiz.direita.inserir_direita(7)
+            if no_atual.direita is not None:
+                fila.put(no_atual.direita)   
+                
+# Exemplo de uso
+arvore = Arvore_Binaria()
+arvore.inserir_em_nivel(4)
+arvore.inserir_em_nivel(2)
+arvore.inserir_em_nivel(6)
+arvore.inserir_em_nivel(1)
+arvore.inserir_em_nivel(3)
+arvore.inserir_em_nivel(5)
+arvore.inserir_em_nivel(7)
 
-# Testando as travessias
-print("Pre-order Traversal:")
-raiz.pre_order()
+print("Pré-ordem:")
+arvore.mostrar_pre_ordem()
+print("\nIn-ordem:")
+arvore.mostrar_in_ordem()
+print("\nPós-ordem:")
+arvore.mostrar_pos_ordem()
+print("\nBFS:")
+arvore.bfs()
 
-print("\nIn-order Traversal:")
-raiz.in_order()
-
-print("\nPost-order Traversal:")
-raiz.pos_order()
-
-print("\nBFS Traversal:")
-raiz.bfs()   
-        
-#Teste
-#Arvore = Arvore_Binaria('a')
-#print(Arvore.valor)
-#print(Arvore.esquerda)
-#print(Arvore.direita)      
-        
 
             
         
